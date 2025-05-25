8b29eb771702646f3b57bba9b7c6234ef16c0826
Ken Krugler <ken@transpac.com>
2025-05-25 10:56:14 -0700

    Remove unused imports to fix build

--- bootcamp/design/src/test/java/com/ververica/flink/training/solutions/BootcampDesignSolutionTest.java
+++ bootcamp/design/src/test/java/com/ververica/flink/training/solutions/BootcampDesignSolutionTest.java
@@ -18,41 +18,9 @@
 
 package com.ververica.flink.training.solutions;
 
-import com.ververica.flink.training.common.*;
-import com.ververica.flink.training.provided.*;
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.serialization.SimpleStringEncoder;
-import org.apache.flink.api.connector.sink2.Sink;
-import org.apache.flink.api.connector.sink2.SinkWriter;
-import org.apache.flink.connector.file.sink.FileSink;
-import org.apache.flink.connector.file.src.FileSource;
-import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
-import org.apache.flink.core.execution.JobClient;
-import org.apache.flink.core.fs.Path;
-import org.apache.flink.streaming.api.CheckpointingMode;
-import org.apache.flink.streaming.api.SourceFunctionTest;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
-import org.apache.flink.streaming.api.functions.sink.v2.DiscardingSink;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-
 import static com.ververica.flink.training.provided.BootcampDesignWorkflowTestUtils.*;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.Assert.assertFalse;
 
 public class BootcampDesignSolutionTest {
 
